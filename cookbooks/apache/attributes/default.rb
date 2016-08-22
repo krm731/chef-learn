c1bd32af4b6b27bc958c2e2b17215a47fb2e5a06
Ram <ram.bcsipl@gmail.com>
2016-08-22 10:55:05 +0000

    chef-repo first commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["ramarkv5"] = { "port" => 80, "domain" => "ramarkv5.mylabserver.com" }
+default["apache"]["sites"]["ramarkv5b"] = { "port" => 80, "domain" => "ramarkv5b.mylabserver.com" }
